8b15045dccd81abcb6baafa434f4bbb1ae67942b
Andreas Färber <afaerber@suse.de>
2019-01-03 16:57:58 +0000

    usb: cdc-acm: Enable serdev support
    Switch from tty_port_register_device() to tty_port_register_device_serdev()
    and from tty_unregister_device() to tty_port_unregister_device().
    
    On removal of a serdev driver sometimes count mismatch warnings were seen:
    
      ttyACM ttyACM0: tty_hangup: tty->count(1) != (#fd's(0) + #kopen's(0))
      ttyACM ttyACM0: tty_port_close_start: tty->count = 1 port count = 0
    
    Note: The serdev drivers appear to probe asynchronously as soon as they
    are registered. Should the USB quirks in probe be moved before registration?
    No noticeable difference for the devices at hand.
    
    Cc: Rob Herring <robh@kernel.org>
    Cc: Johan Hovold <johan@kernel.org>
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- drivers/usb/class/cdc-acm.c
+++ drivers/usb/class/cdc-acm.c
@@ -1460,7 +1460,7 @@ skip_countries:
 	usb_set_intfdata(data_interface, acm);
 
 	usb_get_intf(control_interface);
-	tty_dev = tty_port_register_device(&acm->port, acm_tty_driver, minor,
+	tty_dev = tty_port_register_device_serdev(&acm->port, acm_tty_driver, minor,
 			&control_interface->dev);
 	if (IS_ERR(tty_dev)) {
 		rv = PTR_ERR(tty_dev);
@@ -1534,7 +1534,7 @@ static void acm_disconnect(struct usb_interface *intf)
 	acm_kill_urbs(acm);
 	cancel_work_sync(&acm->work);
 
-	tty_unregister_device(acm_tty_driver, acm->minor);
+	tty_port_unregister_device(&acm->port, acm_tty_driver, acm->minor);
 
 	usb_free_urb(acm->ctrlurb);
 	for (i = 0; i < ACM_NW; i++)
